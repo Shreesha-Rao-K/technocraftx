d3b40627a090436821034ff94e0a687674c69556
Shreesha-Rao-K <shreesharao2011@gmail.com>
2025-09-10 08:45:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        🚀 Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
